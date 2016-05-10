0db477f340db90d838a99f80fc99b43ac18a6bf0
Michael Prather <mprather1@gmail.com>
2016-05-10 00:08:13 -0500

    working  service

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var myApp = angular.module('myApp', [
   'artistControllers'
 ]);
 
-myApp.config(['$routeProvider', function($routeProvider){
+myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
   when('/list', {
     templateUrl: 'partials/list.html',
--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,9 @@
-var artistControllers = angular.module('artistControllers', []);
+var artistControllers = angular.module('artistControllers', [])
 
 artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
-  $http.get('js/data.json').success(function(data) {
+  $http.get('js/data.json').success(function(data){
     $scope.artists = data;
     $scope.artistOrder = 'name';
   });
-}]);
+
+}] );
