b59e632b2959badd94c776e9d9c5763947e28968
Michael Prather <mprather1@gmail.com>
2016-05-09 21:22:01 -0500

    not working

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,8 @@
-var myNameSpace = angular.module('myApp', [])
+var artistControllers = angular.module('artistControllers', []);
 
-myNameSpace.controller('MyController', ['$scope', '$http', function($scope, $http) {
-  $http.get('js/data.json').success(function(data){
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+  $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
     $scope.artistOrder = 'name';
   });
-
-}] );
+}]);
